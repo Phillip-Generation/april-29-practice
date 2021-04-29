726ab6ab6198db9c3e9c9c25b5555673b37376b8
Phillip_Gen <phillip.kinney@generation.org>
2021-04-29 11:47:12 -0400

    functions and for loop

---
+++ resources/app.js
@@ -0,0 +1,26 @@
+//write a function that takes in 2 arguments, and produce the area
+
+// function getArea(length, width){
+//     return length * width;
+  
+// }
+//  console.log(getArea(5,5));
+
+
+
+// const getArea = (length, width) =>{
+//     return length * width;
+// }
+// console.log(getArea(10,5));
+
+// const getArea = (length, width) => length * width;
+// console.log(getArea(8,9));
+
+// const getName = name =>{
+//     console.log('My name is ' + name + '.');
+// }
+// getName('Elizabeth');
+
+// for(let i = 10; i > 0; i--){
+//     console.log(i);
+// }
